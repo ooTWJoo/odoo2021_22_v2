76ff0906a14e2c5aa0dc5feede510cb80ca77384
ooTWJoo <thomassix@hotmail.es>
2022-02-01 14:56:31 +0000

    premium_01/02/22

--- sevenseeds/models/premium.py
+++ sevenseeds/models/premium.py
@@ -19,3 +19,27 @@ class product_premium(models.Model):
     is_premium = fields.Boolean(default=False)
 
     max_characters = fields.Integer()
+
+class sale_premium(models.Model):
+    _name = 'sale.order'
+    _inherit = 'sale.order'
+
+    premium_applied = fields.Boolean(default=False)
+
+    def apply_premium(self):
+        premium_products = self.order_line.filtered(
+            lambda p: p.product_id.is_premium == True and self.premium_applied == False)
+        for p in premium_products:
+            self.partner_id.apply_premium(p.product_id.days_premium)
+        self.premium_applied = True
+
+    def write(self, values):
+        super(sale_premium, self).write(values)
+        self.apply_premium()
+
+    @api.model
+    def create(self, values):
+        record = super(sale_premium, self).create(values)
+        record.apply_premium()
+        return 
+
